7315fc87640adf97b45ce16e9e9adadbeb43ff0a
liuwk <1834183435@qq.com>
2019-01-05 16:58:53 +0800

    123

---
+++ components/public/header/geo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"/> 北京市
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity">
+      切换城市</nuxt-link>
+    [天津 上海 深圳]
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ''
+  }
+</script>
+
+<style>
+</style>
