6cb3c85a2a1154bec735f834d3d4b651ef83f506
“liuwk <1834183435@qq.com>
2019-01-16 16:04:55 +0800

    切换城市页 接口

--- pages/changeCity.vue
+++ pages/changeCity.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="page-changeCity">
     <el-row>
-      <el-col :span="24">1</el-col>
+      <el-col :span="24">
+        <iSelect />
+      </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">2</el-col>
+      <el-col :span="24">
+        <hot />
+      </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">3</el-col>
+      <el-col :span="24">
+        <categroy />
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+  import iSelect from '../components/changeCity/iselect'
+  import hot from '../components/changeCity/hot'
+  import categroy from '../components/changeCity/categroy'
   export default {
+    components:{
+      iSelect,
+      hot,
+      categroy
+    }
   }
 </script>
 
